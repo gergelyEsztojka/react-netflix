b52b9a9d8bf5220bd388a123c7d3e62af81aecd5
Esztojka Gergely <esztojka.gergely@gmail.com>
2021-05-17 15:55:27 +0200

    git init

---
+++ src/components/header/index.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import { Link as ReactRouterLink } from "react-router-dom";
+
+import {
+  Background,
+  Container,
+  Link,
+  Logo,
+  Profile,
+  Dropdown,
+  Picture,
+  Group,
+  ButtonLink,
+  Feature,
+  FeatureCallOut,
+  Text,
+  Search,
+  SearchIcon,
+  SearchInput,
+  PlayButton,
+} from "./styles/header";
+
+export default function Header({ bg = true, children, ...restProps }) {
+  return bg ? <Background {...restProps}>{children}</Background> : children;
+}
+
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+  return <Feature {...restProps}>{children}</Feature>;
+};
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({
+  children,
+  ...restProps
+}) {
+  return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+};
+
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+  return <Profile {...restProps}>{children}</Profile>;
+};
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+  return <Dropdown {...restProps}>{children}</Dropdown>;
+};
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
+
+Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
+  return <Link {...restProps}>{children}</Link>;
+};
+
+Header.Frame = function HeaderFrame({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};
+
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+  return <Group {...restProps}>{children}</Group>;
+};
+
+Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
+  return <ButtonLink {...restProps}>{children}</ButtonLink>;
+};
+
+Header.PlayButton = function HeaderPlayButton({ children, ...restProps }) {
+  return <PlayButton {...restProps}>{children}</PlayButton>;
+};
+
+Header.Search = function HeaderSearchTerm({
+  searchTerm,
+  setSearchTerm,
+  ...restProps
+}) {
+  const [isSearchActive, setIsSearchActive] = useState(false);
+
+  return (
+    <Search {...restProps}>
+      <SearchIcon
+        onClick={() => setIsSearchActive((isSearchActive) => !isSearchActive)}
+      >
+        <img src="/images/icons/search.png" alt="Search" />
+      </SearchIcon>
+      <SearchInput
+        value={searchTerm}
+        onChange={({ target }) => setSearchTerm(target.value)}
+        placeholder="Search films and series"
+        active={isSearchActive}
+      />
+    </Search>
+  );
+};
+
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+  return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+};
+
+Header.Logo = function HeaderLogo({ to, ...restProps }) {
+  return (
+    <ReactRouterLink to={to}>
+      <Logo {...restProps} />
+    </ReactRouterLink>
+  );
+};
